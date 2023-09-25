594ae4f842abc7849d2bf64af212fa4828fcc386
Cozta1 <zkozta@gmail.com>
2023-09-25 12:05:06 -0300

    Lista3

--- Lista3/Exer18.py
+++ Lista3/Exer18.py
@@ -1,37 +1,17 @@
-perfeito1 = 0
-
+num_perfeito = 0
 numero = 1
 
-while perfeito1 < 3:
+while num_perfeito < 3:
     soma = 0
-    for i in range(1, numero + 1):
-        
+    numero+=1
+    for i in range(1, numero):
         if numero % i == 0:
-            # soma += i
-            divisor = numero / i
-            soma += divisor
-            print(divisor)
-            
-        perfeito = soma - numero == numero
-        if  perfeito:
-            perfeito1 += 1
-            print(f'O numero {numero} é perfeito')
-        numero += 1    
-        
+            soma += i
+            # print(i)
+    
+    if  soma == numero:
+        num_perfeito += 1
+        print(f'O numero {numero} é perfeito')
+    numero += 1
 
-       
-        
-        
-        
-#         if soma == numero:
-#     if num_perfeitos_encontrados == 0:
-#         print(f'O 1º número perfeito é: {numero}')
-#     elif num_perfeitos_encontrados == 1:
-#         print(f'O 2º número perfeito é: {numero}')
-#     else:
-#         print(f'O 3º número perfeito é: {numero}')
-#     num_perfeitos_encontrados += 1
-# numero += 1
-
-
-
+# ESSE DEU UM TRABALHO DESGRACADO JUSUS CRISTO
